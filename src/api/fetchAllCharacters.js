6ada13ffdba9723acf602108d362c3febce6cf6a
romanfadeew <romanfadeew@yamoney.ru>
2020-04-15 15:54:23 +0300

    Add mocks for requests

--- src/api/fetchAllCharacters.js
+++ src/api/fetchAllCharacters.js
@@ -4,13 +4,14 @@ import asyncAxios from '../utilities/asyncAxios';
 import tryCatch from 'crocks/Result/tryCatch';
 import characterContract from '../core/contracts/character';
 import resultToAsync from 'crocks/Async/resultToAsync';
+import {characters} from "../configs/apiRoutes";
 
 const mapCharacterList = map(mapProps(characterContract));
 const catchedMapCharacterList = tryCatch(mapCharacterList);
 
 const fetchAllCharacters = asyncAxios({
     method: 'get',
-    url: 'characters',
+    url: characters,
 }).chain(resultToAsync(catchedMapCharacterList));
 
 export default fetchAllCharacters;
--- src/configs/axios.js
+++ src/configs/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { Deserializer } from 'jsonapi-serializer';
 import { fromPromise } from 'crocks/Async';
 import composeB from 'crocks/combinators/composeB';
+import setProp from 'crocks/helpers/setProp';
 
 const { deserialize } = new Deserializer({keyForAttribute: 'camelCase'});
 
@@ -9,9 +10,13 @@ const JSONAPIAdapter = composeB(deserialize, JSON.parse);
 
 export default function initAxiosConfig() {
     axios.defaults.baseURL = 'http://localhost:8080';
+    if (process.env.REACT_APP_MOCK_ON) {
+        axios.defaults.baseURL = '/mocks';
+        axios.interceptors.request.use(res => setProp('url', res.url + '.json', res));
+    }
     axios.defaults.validateStatus = (status) => status < 400;
     axios.defaults.timeout = 5000;
     axios.defaults.transformResponse = [
         fromPromise(JSONAPIAdapter)
-    ]
+    ];
 }
