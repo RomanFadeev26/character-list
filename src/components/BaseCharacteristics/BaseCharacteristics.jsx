ef43aae23e021082503487fecd3468e0b4ffbebe
romanfadeew <romanfadeew@yamoney.ru>
2020-09-10 11:18:36 +0300

    Добавляет базовые характеристики

--- src/components/BaseCharacteristics/BaseCharacteristics.jsx
+++ src/components/BaseCharacteristics/BaseCharacteristics.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import fst from 'crocks/Pair/fst';
 import snd from 'crocks/Pair/snd';
-import {connect} from 'react-redux';
-import { withRouter } from 'react-router';
-import baseCharacteristics from "../../store/selectors/baseCharacteristics";
+import toPairs from 'crocks/Pair/toPairs';
+import listToArray from 'crocks/List/listToArray';
+import {map, merge} from 'crocks/pointfree'
 import composeB from 'crocks/combinators/composeB';
+import pick from 'crocks/helpers/pick';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
+import baseCharacteristics from '../../store/selectors/baseCharacteristics';
+import classes from './BaseCharacteristics.module.css';
 
-const BaseCharacteristics = ({
-    strength,
-    dexterity,
-    constitution,
-    intelligence,
-    wisdom,
-    charisma
-}) => (
-    <div>
-        <div>Сила: {fst(strength)} Модификатор: {snd(strength)}</div>
-        <div>Ловкость: {fst(dexterity)} Модификатор: {snd(dexterity)}</div>
-        <div>Телосложение: {fst(constitution)} Модификатор: {snd(constitution)}</div>
-        <div>Интеллект: {fst(intelligence)} Модификатор: {snd(intelligence)}</div>
-        <div>Мудрость: {fst(wisdom)} Модификатор: {snd(wisdom)}</div>
-        <div>Харизма: {fst(charisma)} Модификатор: {snd(charisma)}</div>
-    </div>
+const baseCharacteristicsText = {
+    strength: 'Сила',
+    dexterity: 'Ловкость',
+    constitution: 'Телосложение',
+    intelligence: 'Интеллект',
+    wisdom: 'Мудрость',
+    charisma: 'Харизма'
+};
+
+const mergerCharacteristicsPair = (charName, charValuePair) => (
+	<li className={classes.CharacteristicBlock}>
+		<div>{fst(charValuePair)}</div>
+		<div>{baseCharacteristicsText[charName]}</div>
+		<div>{snd(charValuePair)}</div>
+	</li>
 );
+
+const characteristicToText = merge(mergerCharacteristicsPair);
+const mapPairsToTexts = composeB(listToArray, map(characteristicToText));
+const keysForProps =  Object.keys(baseCharacteristicsText);
+const propsToPairs = composeB(toPairs, pick(keysForProps));
+const propsToText = composeB(mapPairsToTexts, propsToPairs);
+
+const BaseCharacteristics = (props) => (<ul className={classes.Characteristics}>{propsToText(props)}</ul>);
 
 const connector = composeB(withRouter, connect(baseCharacteristics));
 
