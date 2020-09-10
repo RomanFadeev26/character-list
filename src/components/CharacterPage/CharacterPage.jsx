3fe7b63350d909d7bc31aa898b80b39b8a94592c
romanfadeew <romanfadeew@yamoney.ru>
2020-09-10 11:18:52 +0300

    Добавляет страницу персонажа

--- src/components/CharacterPage/CharacterPage.jsx
+++ src/components/CharacterPage/CharacterPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
-import BaseCharacteristics from "../BaseCharacteristics";
+import {withRouter} from 'react-router';
+import BaseCharacteristics from '../BaseCharacteristics';
+import {fetchCharacterAction} from '../../store/entities/characters/actions';
+import {connect} from 'react-redux';
+import Perks from '../Perks';
+import {composeB} from 'crocks/combinators';
+import classes from './CharacterPage.module.css';
 
-const CharacterPage = () => (<div><BaseCharacteristics /></div>);
+class CharacterPage extends React.Component{
+    componentDidMount() {
+        const { fetchCharacter } = this.props;
+        const { id } = this.props.match.params;
+        fetchCharacter(id);
+    }
 
-export default CharacterPage;
+    render() {
+        return (<div className={classes.CharacterPage}><BaseCharacteristics /><Perks /></div>);
+    }
+}
+
+const connector = composeB(withRouter, connect(null, {fetchCharacter: fetchCharacterAction}));
+
+export default connector(CharacterPage);
